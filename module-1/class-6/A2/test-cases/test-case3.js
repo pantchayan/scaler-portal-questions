ff5b8ea28cb4dd7e8a6d43e646f33a109f7db7f4
Chayan Pant <49815429+pantchayan@users.noreply.github.com>
2023-03-27 05:07:37 +0530

    Update test-case3.js

--- module-1/class-6/A2/test-cases/test-case3.js
+++ module-1/class-6/A2/test-cases/test-case3.js
@@ -22,7 +22,7 @@ test("Verify that content inside pin_container is centered using justify-content
   const body = await page.$("body");
 
   const check = await page.evaluate((body) => {
-    let container = body.querySelector(".pin-container");
+    let container = body.querySelector(".pin_container");
 
     return window.getComputedStyle(container).justifyContent === "center";
   }, body);
