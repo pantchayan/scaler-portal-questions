949c93c3124de8805fb1b2f00351f64bfec8a259
pantchayan <pantchayan@gmail.com>
2023-06-02 01:19:49 +0530

    H1 to L1 in JS

---
+++ fullstack-intermediate/Module 2 - JS/lecture-1 Refresher/Homework/H1 - Decimal to Binary/test-cases/test-case2.js
@@ -0,0 +1,41 @@
+// Description:
+// Sample Test Case 2
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Sample Test Case 2", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate((html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    // Create a function and pass it the input array
+    let runCode = new Function("dec", functionBody);
+
+    // Call the function with the input array
+    let result = runCode(86);
+    // let scriptContent = html.querySelector("body").innerText;
+    return result == "1010110";
+  }, html);
+  expect(check).toBeTruthy();
+});
---
+++ fullstack-intermediate/Module 2 - JS/lecture-1 Refresher/Homework/H1 - Decimal to Binary/test-cases/test-case3.js
@@ -0,0 +1,42 @@
+// Description:
+// Hidden Test Case 1
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Hidden Test Case 1", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate((html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    
+    // Create a function and pass it the input array
+    let runCode = new Function("dec", functionBody);
+
+    // Call the function with the input array
+    let result = runCode(3672);
+    // let scriptContent = html.querySelector("body").innerText;
+    return result == "111001011000";
+  }, html);
+  expect(check).toBeTruthy();
+});
