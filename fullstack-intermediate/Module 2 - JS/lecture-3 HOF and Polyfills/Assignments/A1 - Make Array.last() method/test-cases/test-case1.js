bee20e70d53f13852b7e1920c6a9669dbdb5cba5
pantchayan <pantchayan@gmail.com>
2023-06-14 14:44:06 +0530

    Added Assignmnet Qs for Lecture 3

---
+++ fullstack-intermediate/Module 2 - JS/lecture-3 HOF and Polyfills/Assignments/A1 - Make Array.last() method/test-cases/test-case1.js
@@ -0,0 +1,49 @@
+// Description:
+// Sample Test Case 1
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Sample Test Case 1", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate((html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    // Create a function and pass it the input array
+    let runCode = new Function("x", "y", "fn", functionBody);
+
+    let x = 3;
+    let y = 4;
+    // Call the function with the input array
+    let result = runCode(x, y, sum);
+
+    let wantedResult = 7;
+    // let scriptContent = html.querySelector("body").innerText;
+    return {
+      result,
+      check: wantedResult === result,
+    };
+  }, html);
+  console.log(check.result)
+  expect(check.check).toBeTruthy();
+});
---
+++ fullstack-intermediate/Module 2 - JS/lecture-3 HOF and Polyfills/Assignments/A1 - Make Array.last() method/test-cases/test-case2.js
@@ -0,0 +1,46 @@
+// Description:
+// Sample Test Case 2
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Hidden Test Case 1", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate((html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    // Create a function and pass it the input array
+    let runCode = new Function("x", "y", "fn", functionBody);
+
+    let x = 3;
+    let y = 4;
+    // Call the function with the input array
+    let result1 = runCode(x, y, sum);
+    let result2 = runCode(x, y, mult);
+    let result3 = runCode(x, y, diff);
+
+    // let scriptContent = html.querySelector("body").innerText;
+    return result1 === 7 && result2 === 12 && result3 === -1;
+  }, html);
+  expect(check).toBeTruthy();
+});
