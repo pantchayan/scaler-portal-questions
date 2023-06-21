27855793fd2df3d4cda554b4a6b17add4ec80556
pantchayan <pantchayan@gmail.com>
2023-06-21 20:59:53 +0530

    Homework questions for JS Lecture 3

---
+++ JS Core/Flatten an Array/test-cases/test-case1.js
@@ -0,0 +1,46 @@
+// Description:
+// Sample Test Case 1
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Hidden Test Case 1", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate((html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    // Create a function and pass it the input array
+    let runCode = new Function("array", functionBody);
+
+    let arr = [1, 2, [3, 4, [5]]];
+    let result = runCode(arr);
+
+    // let scriptContent = html.querySelector("body").innerText;
+    return {
+      check: JSON.stringify(result) === JSON.stringify([1, 2, 3, 4, 5]),
+    };
+  }, html);
+  // console.log(check.copiedObject);
+  // console.log(check.originalObject);
+  expect(check.check).toBeTruthy();
+});
---
+++ JS Core/Polyfill Call/test-cases/test-case4.js
@@ -0,0 +1,55 @@
+// Description:
+// Hidden Test Case 3
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Hidden Test Case 3", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate((html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    // Create a function and pass it the input array
+    let runCode = new Function("obj", functionBody);
+    Function.prototype.customBind = runCode;
+
+    function greet() {
+      return `Hello, ${this.name}! of age ${this.age} from ${this.country}`;
+    }
+
+    const person = {
+      name: "John",
+      age: 20,
+      country: "USA",
+    };
+
+    const result = greet.customCall(person);
+
+    // let scriptContent = html.querySelector("body").innerText;
+    return {
+      result,
+      check: result === "Hello, John! of age 20 from USA",
+    };
+  }, html);
+  expect(check.check).toBeTruthy();
+});
