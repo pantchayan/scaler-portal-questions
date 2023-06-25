26d364b92657997533f412f086ee594bbd2d9511
pantchayan <pantchayan@gmail.com>
2023-06-25 22:23:48 +0530

    Added Flatten Object Question

---
+++ JS Core/Flatten an Object/test-cases/test-case1.js
@@ -0,0 +1,64 @@
+// Description:
+// Sample Test Case 1
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Sample Test Case 1", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate((html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    // Create a function and pass it the input array
+    let runCode = new Function("obj", functionBody);
+    const obj = {
+      newObj: {
+        obj2: {
+          obj5: {
+            one: 1,
+          },
+        },
+      },
+      obj3: {
+        obj4: {
+          two: 2,
+        },
+      },
+    };
+
+    let expectedObj = {
+      "newObj.obj2.obj5.one": 1,
+      "obj3.obj4.two": 2,
+    };
+
+    let result = runCode(obj);
+
+    // let scriptContent = html.querySelector("body").innerText;
+    return {
+      check: JSON.stringify(result) === JSON.stringify(expectedObj),
+    };
+  }, html);
+  // console.log(check.copiedObject);
+  // console.log(check.originalObject);
+  expect(check.check).toBeTruthy();
+});
