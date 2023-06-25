c6b6cab3b02309c3d58bb0f603393ae5083abaf7
pantchayan <pantchayan@gmail.com>
2023-06-26 00:12:38 +0530

    adding Currying problems

---
+++ JS Core/Function Curry 2/test-cases/test-case3.js
@@ -0,0 +1,45 @@
+// Description:
+// Sample Test Case 1
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Hidden Test Case 2", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate((html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    // Create a function and pass it the input array
+    let runCode = new Function("y", "x", functionBody);
+    let result1 = runCode(10, 7);
+    let result2 = runCode(10)(7);
+
+    // let scriptContent = html.querySelector("body").innerText;
+    return {
+      check: result1 === 70 && result2 === 70,
+    };
+  }, html);
+  // console.log(check.copiedObject);
+  // console.log(check.originalObject);
+  expect(check.check).toBeTruthy();
+});
