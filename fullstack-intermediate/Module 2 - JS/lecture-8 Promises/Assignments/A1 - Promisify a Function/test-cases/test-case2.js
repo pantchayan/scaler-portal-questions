595f22f21feedc9554fecb6c488436a311bf260e
pantchayan <pantchayan@gmail.com>
2023-07-05 13:48:26 +0530

    Added Assignment to Lecture 8

---
+++ fullstack-intermediate/Module 2 - JS/lecture-8 Promises/Assignments/A1 - Promisify a Function/test-cases/test-case2.js
@@ -0,0 +1,51 @@
+// Description:
+// Hidden Test Case 1
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Hidden Test Case 1", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate(async (html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    // Create a function and pass it the input array
+    let runCode = new Function("fn", functionBody);
+
+    const exampleFn = function (a, b, cb) {
+      cb(a * b);
+    };
+    const promisified = runCode(exampleFn);
+
+    let result;
+    promisified(5, 15).then((res) => {
+      result = res;
+    });
+
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+    await delay(1000);
+    // let scriptContent = html.querySelector("body").innerText;
+    return result === 75;
+  }, html);
+  expect(check).toBeTruthy();
+});
---
+++ fullstack-intermediate/Module 2 - JS/lecture-8 Promises/Homework/H1 - Two Files in Series by Promise/test-cases/test-case2.js
@@ -0,0 +1,51 @@
+// Description:
+// Hidden Test Case 1
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Hidden Test Case 1", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate(async (html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    // Create a function and pass it the input array
+    let runCode = new Function("file1", "file2", "ansArray", functionBody);
+
+    // Call the function with the input array
+    let ansArr = [];
+    runCode("FILE A", "FILE B", ansArr);
+
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+    await delay(1000);
+
+    let expectedArr = [
+      "content : FILE A",
+      "content : FILE B",
+      "All files have been read",
+    ];
+    // let scriptContent = html.querySelector("body").innerText;
+    return JSON.stringify(ansArr) == JSON.stringify(expectedArr);
+  }, html);
+  expect(check).toBeTruthy();
+});
