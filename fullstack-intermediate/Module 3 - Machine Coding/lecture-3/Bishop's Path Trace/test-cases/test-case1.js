d262070bed057895c03c8a2f2b519eef7a0fa253
pantchayan <pantchayan@gmail.com>
2023-07-30 17:30:16 +0530

    added chess questions

---
+++ fullstack-intermediate/Module 3 - Machine Coding/lecture-3/Bishop's Path Trace/test-cases/test-case1.js
@@ -0,0 +1,75 @@
+// Description:
+// Verify that the color of the card changes when double clicked (class of desired color is added)
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("clicking on a checkbox, the program traces the path of Bishop from that checkbox.", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const bodyHandle = await page.$("body");
+
+  const checkbox = await page.$("#\\31 1");
+  await checkbox.click();
+
+  const check = await page.evaluate((body) => {
+    let checkBoxesDiv = body.querySelectorAll("div.checkbox");
+    let arr = [];
+    function isCheckboxRed(idx) {
+      if (checkBoxesDiv[idx].style.backgroundColor == "red") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function checkBishopTraceByIndex(idx) {
+      let directionArr = [
+        [1, 1],
+        [-1, -1],
+        [-1, 1],
+        [1, -1],
+      ];
+      let flag = true;
+      if (!isCheckboxRed(idx)) {
+        return false;
+      }
+      for (let di = 0; di < directionArr.length; di++) {
+        let dr = directionArr[di][0];
+        let dc = directionArr[di][1];
+
+        let ri = Math.floor(idx / 8);
+        let ci = idx - ri * 8;
+        // idx = (row * 8) + col + 1;
+        while (ri + dr < 8 && ri + dr >= 0 && ci + dc < 8 && ci + dc >= 0) {
+          ri = ri + dr;
+          ci = ci + dc;
+          if (!isCheckboxRed(ri * 8 + ci)) {
+            arr.push((ri * 8 + ci) + " isnt red");
+            return false;
+          } else {
+            arr.push("checked " + (ri * 8 + ci));
+          }
+        }
+      }
+      return true;
+    }
+
+    let flag = checkBishopTraceByIndex(10);
+    return { flag, arr };
+  }, bodyHandle);
+  console.log(check.arr);
+  expect(check.flag).toBeTruthy();
+});
