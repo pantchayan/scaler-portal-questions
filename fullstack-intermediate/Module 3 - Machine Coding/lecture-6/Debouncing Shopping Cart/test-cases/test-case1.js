0a6dc6885517d6552996311b5b677a4e37469617
pantchayan <pantchayan@gmail.com>
2023-08-08 14:49:25 +0530

    debouncing shopping cart

---
+++ fullstack-intermediate/Module 3 - Machine Coding/lecture-6/Debouncing Shopping Cart/test-cases/test-case1.js
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Rapidly clicking multiple times on an item, adds it just once.", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const bodyHandle = await page.$("body");
+  // Get the bounding box of the element to perform the double-click
+  const elementHandle = await page.$(
+    "body > div.container > div:nth-child(4) > button"
+  );
+
+  await elementHandle.click({ clickCount: 2 });
+  // const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  // await delay(1000);
+  await elementHandle.click({ clickCount: 2 });
+
+  const check = await page.evaluate(async (body) => {
+    let span = body.querySelector("body > div.cart-total > p > span");
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+    await delay(1000);
+
+    return { total: span.innerHTML, flag: span.innerHTML === "400" };
+  }, bodyHandle);
+  console.log(check.total);
+  expect(check.flag).toBeTruthy();
+});
