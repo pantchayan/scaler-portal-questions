6c253eef867bd3853525eea13baa47957821d949
pantchayan <pantchayan@gmail.com>
2023-10-09 07:30:47 +0530

    3 Questions to center the div

---
+++ SST Problems/Center-Div/Center a div using Grid property/test-cases/test-case1.js
@@ -0,0 +1,31 @@
+// Description:
+// Verify if the following property available on .grid class grid-template-columns: repeat(3, 1fr) or 3fr 3fr 3fr;
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Verify that the div is centered using the mentioned properties.", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const body = await page.$("body");
+
+  const check = await page.evaluate((body) => {
+    let div = window.getComputedStyle(body.querySelector(".parent"));
+
+    return div.display === "grid" && div.placeItems === "center";
+  }, body);
+
+  expect(check).toBeTruthy();
+});
---
+++ SST Problems/Center-Div/Center a div using Table Cell property/test-cases/test-case1.js
@@ -0,0 +1,37 @@
+// Description:
+// Verify if the following property available on .grid class grid-template-columns: repeat(3, 1fr) or 3fr 3fr 3fr;
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Verify that the div is centered using the mentioned properties.", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const body = await page.$("body");
+
+  const check = await page.evaluate((body) => {
+    let div = window.getComputedStyle(body.querySelector(".parent"));
+    let childDiv = window.getComputedStyle(body.querySelector(".child"));
+
+    return (
+      div.display === "table-cell" &&
+      div.verticalAlign === "middle" &&
+      div.textAlign === "center" &&
+      childDiv.display === "inline-block"
+    );
+  }, body);
+
+  expect(check).toBeTruthy();
+});
