fe9d8d1f029ffd19ab5c4131137be2fae5f0b7a8
pantchayan <pantchayan@gmail.com>
2024-01-09 12:22:57 +0530

    Question for async-2

---
+++ frontend-beginner/async-2/Question-2/test-cases/test-case1.js
@@ -0,0 +1,53 @@
+// Description:
+// Sample Test Case 1
+const puppeteer = require("puppeteer");
+
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ["--no-sandbox"], // This was important. Can't remember why
+  });
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+test("Test Case 1", async () => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+
+  const html = await page.$("html");
+
+  const check = await page.evaluate(async (html) => {
+    let scriptContent = html.querySelector("#solution").innerHTML;
+
+    let functionBody = scriptContent.substring(
+      scriptContent.indexOf("{") + 1,
+      scriptContent.lastIndexOf("}")
+    );
+
+    let runCode = new Function("delay", functionBody);
+
+    // t = 0:
+
+    let str = "";
+    // Test Cases
+    await runCode(1000)
+      .then((result) => (str += result)) // Expected output after 2000ms: "Promise resolved after 1000 milliseconds"
+      .catch((error) => (str += error));
+
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+    await delay(1000);
+
+    return {
+      ans: str,
+      flag: str === "Promise resolved after 1000 milliseconds",
+    };
+  }, html);
+  console.log(check.ans);
+  expect(check.flag).toBeTruthy();
+});
